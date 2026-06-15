7652956b1ed225a49051d82aad4c66ca72bc3032
irkalini <irakalina2004@gmail.com>
2026-06-15 15:57:17 +0200

    ex06 verified

--- src/ex06.py
+++ src/ex06.py
@@ -5,27 +5,6 @@ from ex00 import test
 
 def main():
     # cross product
-    test("Cross product basis i x j", lambda: (
-        cross_product(
-            Vector([1.0, 0.0, 0.0]),
-            Vector([0.0, 1.0, 0.0])
-        ).print()
-    ))  # [0, 0, 1]
-
-    test("Cross product basis j x k", lambda: (
-        cross_product(
-            Vector([0.0, 1.0, 0.0]),
-            Vector([0.0, 0.0, 1.0])
-        ).print()
-    ))  # [1, 0, 0]
-
-    test("Cross product basis k x i", lambda: (
-        cross_product(
-            Vector([0.0, 0.0, 1.0]),
-            Vector([1.0, 0.0, 0.0])
-        ).print()
-    ))  # [0, 1, 0]
-
     test("Cross product subject example 1", lambda: (
         cross_product(
             Vector([0.0, 0.0, 1.0]),
@@ -39,6 +18,13 @@ def main():
             Vector([4.0, 5.0, 6.0])
         ).print()
     ))  # [-3, 6, -3]
+
+    test("Cross product subject example 3", lambda: (
+        cross_product(
+            Vector([4.0, 2.0, -3.0]),
+            Vector([-2.0, -5.0, 16.0])
+        ).print()
+    ))  # [17, -58, -16]
 
     test("Cross product identical vectors", lambda: (
         cross_product(
