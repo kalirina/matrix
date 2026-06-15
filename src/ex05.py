1cd9667f28b3e8d6cc8d2285edf2975f37f1e287
irkalini <irakalina2004@gmail.com>
2026-06-15 15:50:18 +0200

    ex05 verified

--- src/ex05.py
+++ src/ex05.py
@@ -11,42 +11,55 @@ def main():
             Vector([1.0, 0.0])
         )
     ))  # 1
+
     test("Angle cosine orthogonal vectors", lambda: print(
         angle_cos(
             Vector([1.0, 0.0]),
             Vector([0.0, 1.0])
         )
     ))  # 0
+
     test("Angle cosine opposite vectors", lambda: print(
         angle_cos(
             Vector([1.0, 0.0]),
             Vector([-1.0, 0.0])
         )
     ))  # -1
+
     test("Angle cosine subject example 1", lambda: print(
         angle_cos(
             Vector([1.0, 0.0]),
             Vector([1.0, 0.0])
         )
     ))  # 1
+
     test("Angle cosine subject example 2", lambda: print(
         angle_cos(
             Vector([1.0, 0.0]),
             Vector([0.0, 1.0])
         )
     ))  # 0
+
     test("Angle cosine subject example 3", lambda: print(
         angle_cos(
             Vector([-1.0, 1.0]),
             Vector([1.0, -1.0])
         )
     ))  # -1
+
+    test("Angle cosine subject example 4", lambda: print(
+        angle_cos(
+            Vector([1, 2, 3]),
+            Vector([4, 5, 6])
+        )
+    ))  # -1
+
     test("Angle cosine arbitrary vectors", lambda: print(
         angle_cos(
             Vector([2.0, 1.0]),
             Vector([4.0, 2.0])
         )
-    ))  # 1
+    ))  # 0.9746
 
     # edges, errors
     test("Angle cosine size mismatch", lambda: print(
@@ -55,12 +68,14 @@ def main():
             Vector([1.0, 2.0, 3.0])
         )
     ))
+
     test("Angle cosine type mismatch", lambda: print(
         angle_cos(
             Vector([1, 2, 3]),
             Vector([1.0, 2.0, 3.0])
         )
     ))
+
     test("Angle cosine zero vector", lambda: print(
         angle_cos(
             Vector([0.0, 0.0]),
