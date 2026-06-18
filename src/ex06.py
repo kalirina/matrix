9c630fb3d6ad71f53cae2296182f0e85c21c651b
irkalini <irakalina2004@gmail.com>
2026-06-18 17:18:32 +0200

    fixed ex06 tests

--- src/ex06.py
+++ src/ex06.py
@@ -6,60 +6,48 @@ from ex00 import test
 def main():
     # cross product
     test("Cross product subject example 1", lambda: (
-        cross_product(
-            Vector([0.0, 0.0, 1.0]),
-            Vector([1.0, 0.0, 0.0])
-        ).print()
-    ))  # [0, 1, 0]
+        res := cross_product(Vector([0.0, 0.0, 1.0]), Vector([1.0, 0.0, 0.0])),
+        res.print(),
+        print("OK" if res.data == [0.0, 1.0, 0.0] else "KO")
+    ))
 
     test("Cross product subject example 2", lambda: (
-        cross_product(
-            Vector([1.0, 2.0, 3.0]),
-            Vector([4.0, 5.0, 6.0])
-        ).print()
-    ))  # [-3, 6, -3]
+        res := cross_product(Vector([1.0, 2.0, 3.0]), Vector([4.0, 5.0, 6.0])),
+        res.print(),
+        print("OK" if res.data == [-3.0, 6.0, -3.0] else "KO")
+    ))
 
     test("Cross product subject example 3", lambda: (
-        cross_product(
-            Vector([4.0, 2.0, -3.0]),
-            Vector([-2.0, -5.0, 16.0])
-        ).print()
-    ))  # [17, -58, -16]
+        res := cross_product(Vector([4.0, 2.0, -3.0]), Vector([-2.0, -5.0, 16.0])),
+        res.print(),
+        print("OK" if res.data == [17.0, -58.0, -16.0] else "KO")
+    ))
 
     test("Cross product identical vectors", lambda: (
-        cross_product(
-            Vector([1.0, 2.0, 3.0]),
-            Vector([1.0, 2.0, 3.0])
-        ).print()
-    ))  # [0, 0, 0]
+        res := cross_product(Vector([1.0, 2.0, 3.0]), Vector([1.0, 2.0, 3.0])),
+        res.print(),
+        print("OK" if res.data == [0.0, 0.0, 0.0] else "KO")
+    ))
 
     test("Cross product parallel vectors", lambda: (
-        cross_product(
-            Vector([1.0, 2.0, 3.0]),
-            Vector([2.0, 4.0, 6.0])
-        ).print()
-    ))  # [0, 0, 0]
+        res := cross_product(Vector([1.0, 2.0, 3.0]), Vector([2.0, 4.0, 6.0])),
+        res.print(),
+        print("OK" if res.data == [0.0, 0.0, 0.0] else "KO")
+    ))
+
+    test("Cross product different types", lambda: (
+        res := cross_product(Vector([1, 2, 3]), Vector([1.0, 2.0, 3.0])),
+        res.print(),
+        print("OK" if res.data == [0.0, 0.0, 0.0] else "KO")
+    ))
 
     # edges, errors
     test("Cross product size mismatch", lambda: (
-        cross_product(
-            Vector([1.0, 2.0, 3.0]),
-            Vector([1.0, 2.0])
-        ).print()
+        cross_product(Vector([1.0, 2.0, 3.0]), Vector([1.0, 2.0])).print()
     ))
 
     test("Cross product not 3D", lambda: (
-        cross_product(
-            Vector([1.0, 2.0]),
-            Vector([3.0, 4.0])
-        ).print()
-    ))
-
-    test("Cross product type mismatch", lambda: (
-        cross_product(
-            Vector([1, 2, 3]),
-            Vector([1.0, 2.0, 3.0])
-        ).print()
+        cross_product(Vector([1.0, 2.0]), Vector([3.0, 4.0])).print()
     ))
 
 
